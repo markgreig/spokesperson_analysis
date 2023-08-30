d4ca6cbca9f463e993339370b731f22c68f43329
markgreig <53443668+markgreig@users.noreply.github.com>
2023-08-30 08:40:32 +0100

    claude tweaks Spokesperson_analysis.py

--- Spokesperson_analysis.py
+++ Spokesperson_analysis.py
@@ -4,38 +4,34 @@
 # In[ ]:
 import re
 import streamlit as st
-
-st.title('My App') 
-
-
 import pandas as pd
 
-text = st.text_input("Paste text here")
+text = st.text_input("Paste text")
 
-# Extract rows
 rows = re.findall(r'([^\d]+ [\d\w]+ [^\d]+)\s(\d+)', text)
 
-# Split spokesperson column 
 data = []
+
 for row in rows:
   spokesperson = row[0].strip()
   spokespeople = spokesperson.split('|')
   spokespeople = [name.strip() for name in spokespeople]
+  
   frequency = int(row[1])
   
   for spokesperson in spokespeople:
     data.append([spokesperson, frequency])
-
-# Create dataframe  
+    
 df = pd.DataFrame(data, columns=['Spokesperson', 'Frequency'])
 
-# Group by Spokesperson and calculate the sum of Frequency
-result = df.groupby('Spokesperson')['Frequency'].sum().reset_index()
+top_spokespeople = df.groupby('Spokesperson')['Frequency'].sum().reset_index()
 
-# Sort by Frequency in descending order
-result = result.sort_values(by='Frequency', ascending=False)
+top_spokespeople = top_spokespeople.sort_values(by='Frequency', ascending=False) 
 
-result.to_csv('top_spokespeople.csv', index=False)
+# Display dataframe  
+st.write(top_spokespeople)
+
+top_spokespeople.to_csv('top_spokespeople.csv', index=False)
 
 
 @st.cache
