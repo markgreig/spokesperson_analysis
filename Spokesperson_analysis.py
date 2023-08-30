a12188ef4e7294d3821cab5f2ba65d750ed35fb8
markgreig <53443668+markgreig@users.noreply.github.com>
2023-08-30 09:35:39 +0100

    Update Spokesperson_analysis.py

--- Spokesperson_analysis.py
+++ Spokesperson_analysis.py
@@ -4,58 +4,56 @@
 # In[ ]:
 import re
 import pandas as pd
+import streamlit as st
 
 text = "NAME1 NAME2 10 \nNAME3|NAME4|NAME5 15"
 
 # Phase 1 - split rows on '|'
-rows = [] 
+rows = []
 for line in text.split('\n'):
-  if '|' in line:
-    names = line.split('|')
-    for name in names:
-      rows.append(name)
-  else:
-    rows.append(line)
+    if '|' in line:
+        names = line.split('|')
+        for name in names:
+            rows.append(name)
+    else:
+        rows.append(line)
 
-print(rows)
-# ['NAME1 NAME2 10', 'NAME3', 'NAME4', 'NAME5 15']
-
-# Phase 2 - extract names and number 
-pattern = '([\w ]+) (\d+)'
+# Phase 2 - extract names and numbers
+pattern = '(\w+ \w+) (\d+)'
 
 data = []
 for row in rows:
-  match = re.search(pattern, row)
-  if match:
-    name = ' '.join(match.group(1).split())
-    number = match.group(2)  
-    data.append([name, number])
+    matches = re.findall(pattern, row)
+    for match in matches:
+        name = match[0]
+        number = int(match[1])  # Convert the frequency to an integer
+        data.append([name, number])
 
-    
+# Create a DataFrame
 df = pd.DataFrame(data, columns=['Spokesperson', 'Frequency'])
 
+# Group and aggregate the data
 top_spokespeople = df.groupby('Spokesperson')['Frequency'].sum().reset_index()
 
-top_spokespeople = top_spokespeople.sort_values(by='Frequency', ascending=False) 
+# Sort the data by frequency in descending order
+top_spokespeople = top_spokespeople.sort_values(by='Frequency', ascending=False)
 
-# Display dataframe  
+# Display the DataFrame using Streamlit
 st.write(top_spokespeople)
 
-top_spokespeople.to_csv('top_spokespeople.csv', index=False)
-
-
+# Download button for CSV
 @st.cache
-def convert_df(result):
-     return result.to_csv().encode('utf-8')
+def convert_df_to_csv(result):
+    return result.to_csv(index=False).encode('utf-8')
 
-csv = convert_df(result)
+csv = convert_df_to_csv(top_spokespeople)
 
 st.download_button(
-     label="Download data as CSV",
-     data=csv,
-     file_name='top_spokespeople.csv',
-     mime='text/csv',
- )
+    label="Download data as CSV",
+    data=csv,
+    file_name='top_spokespeople.csv',
+    mime='text/csv',
+)
 
 # In[ ]:
 
